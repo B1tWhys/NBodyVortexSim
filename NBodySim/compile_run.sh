8045ea1b05e3ae951dbbd2d232045576cf308fdb
bouldersky <skyarnold1@me.com>
2018-07-02 18:58:55 -0600

    added script to streamline running the sim and converting the output images to video

---
+++ NBodySim/compile_run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export debug=false
+./compile.sh &&
+
+rm ./outputImages/*.png
+rm ./outputImages/*.mp4
+
+./simulator
+
+cd ./outputImages
+
+ffmpeg -f image2 -r 24 -pattern_type glob -i '*.png' output.mp4
+# rm ./*.png
+
+open ./output.mp4
